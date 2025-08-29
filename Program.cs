14f79d2879fb352e76d54bc701ea6c79e6a5263a
SpencerSolt <spencer.solt@students.snow.edu>
2025-08-29 12:12:48 -0600

    finished step:2

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
-﻿//Explore integar math
-int a = 18;
-int b = 6;
-int c = a + b;
-Console.WriteLine(c);
-c = a - b;
-Console.WriteLine(c);
-c = a * b;
-Console.WriteLine(c);
-c = a / b;
-Console.WriteLine(c);
+﻿//Explore order of operations
+int a = 5;
+int b = 4;
+int c = 2;
+int d = a + b * c;
+Console.WriteLine(d);
+d = (a + b) * c;
+Console.WriteLine(d);
+d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+Console.WriteLine(d);
+a = 7;
+b = 4;
+c = 3;
+d = (a + b) / c;
+Console.WriteLine(d);
