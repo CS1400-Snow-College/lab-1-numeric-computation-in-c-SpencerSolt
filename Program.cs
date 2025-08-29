09cf0dd087beb8e84bbfc060e3e80df3c34db4f8
SpencerSolt <spencer.solt@students.snow.edu>
2025-08-29 12:15:48 -0600

    finished step:3

--- Program.cs
+++ Program.cs
@@ -1,15 +1,13 @@
-﻿//Explore order of operations
-int a = 5;
+﻿//Explore intergar precision limits
+int a = 7;
 int b = 4;
-int c = 2;
-int d = a + b * c;
-Console.WriteLine(d);
-d = (a + b) * c;
-Console.WriteLine(d);
-d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
-Console.WriteLine(d);
-a = 7;
-b = 4;
-c = 3;
-d = (a + b) / c;
-Console.WriteLine(d);
+int c = 3;
+int d = (a + b) / c;
+int e = (a + b) % c;
+Console.WriteLine($"quotient: {d}");
+Console.WriteLine($"remainder: {e}");
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The range of integers is {min} to {max}");
+int what = max + 3;
+Console.WriteLine($"An example of overflow: {what}");
